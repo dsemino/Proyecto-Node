85ee7d0985203c4d5aa3cb76c109885fc25fe05b
Lenovo14\Dario <dsemino@icpayment.net>
2026-05-02 18:28:12 -0300

    Primer commit proyecto node

---
+++ index.js
@@ -0,0 +1,79 @@
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+import  {argv } from 'process';
+
+const comando = argv[2];
+const parametro1 = argv[3];
+const parametro2 = argv[4];
+const parametro3 = argv[5];
+const parametro4 = argv[6];
+let product;
+let partes;
+let id;
+let url;
+
+switch(comando) {
+    case 'GET':
+        //console.log(`Toma un dato`);
+        if (!parametro1){
+            fetch('https://fakestoreapi.com/products')
+            
+            .then(response => response.json())
+            .then(data => console.log(data));
+        }
+        else{
+            partes = parametro1.split("products");
+            // partes[0] sería lo que está antes ("")
+            // partes[1] es lo que está después ("123")
+            id = partes[1]; 
+            url = "https://fakestoreapi.com/products" + id;
+            fetch(url)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch((error) => console.log(error));
+        }
+        break;
+    case 'POST':
+        //console.log(`Recibimos ${parametro} satisfactoriamente`);
+        product = { title: parametro2, price: parametro3, category: parametro4 };
+        fetch('https://fakestoreapi.com/products', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(product)
+            })
+        .then(response => response.json())
+        .then(data => console.log(data));
+        break;
+    case 'PUT':
+        //console.log(`Modificamos el item con id : ${parametro} satisfactoriamente`);
+        // product = { title: parametro2, price: parametro3, category: parametro4 };
+        // fetch('https://fakestoreapi.com/products/1', {
+        // method: 'PUT',
+        // headers: { 'Content-Type': 'application/json' },
+        // body: JSON.stringify(product)
+        // })
+        // .then(response => response.json())
+        // .then(data => console.log(data)); 
+        break;
+    case 'DELETE':
+        //console.log(`El item con id : ${parametro} se eliminó con exito`);
+        partes = parametro1.split("products");
+        // partes[0] sería lo que está antes ("")
+        // partes[1] es lo que está después ("123")
+        id = partes[1]; 
+        url = "https://fakestoreapi.com/products" + id;
+        console.log(url);
+        fetch(url,{
+            method: 'DELETE'
+            })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch((error) => console.log(error));
+        break;
+
+}
